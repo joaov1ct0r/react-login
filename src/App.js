1b8310271affcf5b25871e5e2a83524149380aba
Joao Victor <jov9051@outlook.com>
2022-02-11 19:27:15 -0300

    LoginContainer sendo utilizado em app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 import LoginContainer from './components/LoginContainer';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <LoginContainer></LoginContainer>
+        </div>
+    );
 }
 
 export default App;
