2ef9e94e299ceb2ab2632b4666a81b29ec435225
Joao Victor <jov9051@outlook.com>
2022-02-11 19:14:59 -0300

    codigo form removido de app.js

--- src/App.js
+++ src/App.js
@@ -5,22 +5,6 @@ function App() {
         <div className="App">
             <main className="login-container">
                 <div className="login">
-                    <form>
-                        <input
-                            type="email"
-                            name="email"
-                            placeholder="Email"
-                            autoFocus
-                        />
-
-                        <input
-                            type="password"
-                            name="password"
-                            placeholder="Senha"
-                        />
-
-                        <button type="submit">Login</button>
-                    </form>
                     <p>
                         Ainda não é cadastrado ? {''}
                         <a href="http://localhost:3000">Cadastre-se</a>
