42c198f0e01965f94ca12c33bd314a8a3303a590
Joao Victor <jov9051@outlook.com>
2022-02-11 19:18:02 -0300

    codigo login movido para componente Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,3 +1,12 @@
-function Login() {}
+function Login() {
+    return (
+        <div className="login">
+            <p>
+                Ainda não é cadastrado ? {''}
+                <a href="http://localhost:3000">Cadastre-se</a>
+            </p>
+        </div>
+    );
+}
 
 export default Login;
