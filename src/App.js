15d20717f92c6d0d0ed9742db94fb99e5378a724
Joao Victor <jov9051@outlook.com>
2022-02-11 19:18:17 -0300

    codigo login removido de app.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,7 @@ function App() {
     return (
         <div className="App">
             <main className="login-container">
-                <div className="login">
-                    <Form></Form>
-                    <p>
-                        Ainda não é cadastrado ? {''}
-                        <a href="http://localhost:3000">Cadastre-se</a>
-                    </p>
-                </div>
+                <Form></Form>
             </main>
         </div>
     );
