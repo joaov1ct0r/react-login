6417fd6891bb1e5318df0fcc1ce8e01e8b03cbcc
Joao Victor <jov9051@outlook.com>
2022-02-11 19:15:46 -0300

    comopnente Form sendo utilizado em app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
         <div className="App">
             <main className="login-container">
                 <div className="login">
+                    <Form></Form>
                     <p>
                         Ainda não é cadastrado ? {''}
                         <a href="http://localhost:3000">Cadastre-se</a>
