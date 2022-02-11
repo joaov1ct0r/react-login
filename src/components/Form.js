2a3f158a9ba3b95df085e3ff08503f31ef333a71
Joao Victor <jov9051@outlook.com>
2022-02-11 19:14:40 -0300

    codigo form movido para componente Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,3 +1,13 @@
-function Form() {}
+function Form() {
+    return (
+        <form>
+            <input type="email" name="email" placeholder="Email" autoFocus />
+
+            <input type="password" name="password" placeholder="Senha" />
+
+            <button type="submit">Login</button>
+        </form>
+    );
+}
 
 export default Form;
