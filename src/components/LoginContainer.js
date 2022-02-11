1078888a22b8329f1bc07fdd1603da6b8646fc41
Joao Victor <jov9051@outlook.com>
2022-02-11 19:25:54 -0300

    componente Login sendo utilizado em LoginContainer.js

--- src/components/LoginContainer.js
+++ src/components/LoginContainer.js
@@ -1,7 +1,11 @@
 import Login from './Login';
 
 function LoginContainer() {
-    return <main className="login-container"></main>;
+    return (
+        <main className="login-container">
+            <Login></Login>
+        </main>
+    );
 }
 
 export default LoginContainer;
